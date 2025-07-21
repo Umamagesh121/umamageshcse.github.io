c3fcee9b8392e121d154ecd5738817ada7865150
UMAMAGESHWARAN <umamageshwaran121@gmail.com>
2025-07-21 23:08:28 +0530

    Add files via upload

---
+++ scripct.js
@@ -0,0 +1,19 @@
+const links = document.querySelectorAll('.nav-link');
+
+window.addEventListener('scroll', () => {
+  let current = '';
+  document.querySelectorAll('section').forEach((section) => {
+    const sectionTop = section.offsetTop - 70;
+    const sectionHeight = section.clientHeight;
+    if (pageYOffset >= sectionTop && pageYOffset < sectionTop + sectionHeight) {
+      current = section.getAttribute('id');
+    }
+  });
+
+  links.forEach((link) => {
+    link.classList.remove('active');
+    if (link.getAttribute('href') === '#' + current) {
+      link.classList.add('active');
+    }
+  });
+});
